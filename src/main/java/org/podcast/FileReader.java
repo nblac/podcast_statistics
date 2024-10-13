417f91350bd19d4ed2643868c43b86d851bdf189
Bogdan Laculiceanu <bogdan.laculiceanu@mercmarine.com>
2024-10-13 14:02:40 +0300

    Move reading file in a separate class

---
+++ src/main/java/org/podcast/FileReader.java
@@ -0,0 +1,43 @@
+package org.podcast;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ * This class reads a large file using Scanner class from specific location,
+ * get each line and transform it into a JsonObject using JsonParser class
+ * and returns an array list of podcasts
+ * @param fileName as string - the file location in txt format
+ * @return          listOfPodcasts - an array list of podcasts
+ */
+
+public final class FileReader {
+    public ArrayList readUsingScanner(String fileName) throws IOException {
+        // define var for file location
+        Path path = Paths.get(fileName);
+        Scanner scanner = new Scanner(path);
+        JsonParser jsonParser = new JsonParser();
+
+        //crete array list object to store podcasts read from file
+        ArrayList<Object> listOfPodcasts = new ArrayList<>();
+
+        //read line by line and store podcasts into an array list
+        while(scanner.hasNextLine()){
+            //process each line and store it in an array list
+            String line = scanner.nextLine();
+            JsonObject jo = (JsonObject)jsonParser.parse(line);
+            listOfPodcasts.add(jo);
+        }
+        // close file scan
+        scanner.close();
+
+        return listOfPodcasts;
+    }
+}
