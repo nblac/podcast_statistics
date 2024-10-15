a3feec67f74a574dd6470779b0579baf5df3c775
Bogdan Laculiceanu <bogdan.laculiceanu@mercmarine.com>
2024-10-15 22:40:35 +0300

    Add  method to return show by opportunities as json and method to return desired string format for opportunities result; add test; fix typo

--- src/test/java/org/podcast/StatisticsTest.java
+++ src/test/java/org/podcast/StatisticsTest.java
@@ -1,34 +1,62 @@
 package org.podcast;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.google.gson.JsonObject;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsTest {
 
-    private static final String fileName = "src/test/resources/downloads.txt";
-    private static Statistics stats;
+    static String fileName = "src/test/resources/downloads.txt";
+    static FileReader fileReader = new FileReader();
+    static ArrayList listOfPodcasts = null;
+    Statistics stats = new Statistics();
 
-    @BeforeEach
-    void setUp() {
-        stats = new Statistics();
+    @BeforeAll
+    public static void setUp() {
+
+        try {
+            listOfPodcasts = fileReader.readUsingScanner(fileName);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void podcastsDownloadsByCityTest() {
+
+        JsonObject listOfPodcastsByDownloads = stats.podcastsDownloadsByCity(listOfPodcasts, "San Francisco");
+        String mostPopularShow = stats.mostPopularShowByDownloads(listOfPodcastsByDownloads);
+        System.out.println(mostPopularShow);
+
+        assertEquals(mostPopularShow, "Most popular show is: Who Trolled Amber\nNumber of downloads is: 24");
+    }
+
+    @Test
+    void podcastsDownloadsByDeviceTest() {
+        JsonObject downloadsByDevice = stats.podcastsDownloadsByDevice(listOfPodcasts);
+        String mostPopularDevice = stats.mostPopularDeviceByDownloads(downloadsByDevice);
+        System.out.println(mostPopularDevice);
+        assertEquals(mostPopularDevice, "Most popular device is: mobiles & tablets\nNumber of downloads is: 60");
     }
 
 
     @Test
-    void podcastsDownloadsByCity() {
-    }
+    void listTheOpportunitiesToInsertCommercial(){
+        String expectedResult = """
+                                    Show Id: Stuff You Should Know, Preroll Opportunity Number: 40
+                                    Show Id: Who Trolled Amber, Preroll Opportunity Number: 40
+                                    Show Id: Crime Junkie, Preroll Opportunity Number: 30
+                                    Show Id: The Joe Rogan Experience, Preroll Opportunity Number: 10
+                                """;
 
-    @Test
-    void podcastsDownloadsByDevice() {
-    }
+        JsonObject opportunitiesToInsertCommercial = stats.opportunitiesToInsertCommercial(listOfPodcasts);
+        JsonObject sortedObject = stats.arrangeOpportunitiesToInsertCommercialHighToLow(opportunitiesToInsertCommercial);
 
-    @Test
-    void mostPopularShowByDownloads() {
-    }
-
-    @Test
-    void mostPopularDeviceByDownloads() {
+        String returnedString =  stats.formatStringsForOpportunitiesToInsertCommercial(sortedObject);
+        assertEquals(expectedResult, returnedString);
     }
 }
