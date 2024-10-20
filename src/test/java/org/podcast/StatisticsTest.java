0a8456d67464fc3ccfa31b1ca50e5aa34e4be71c
Bogdan Laculiceanu <bogdan.laculiceanu@mercmarine.com>
2024-10-20 12:50:18 +0300

    Add test class for weekly shows

--- src/test/java/org/podcast/StatisticsTest.java
+++ src/test/java/org/podcast/StatisticsTest.java
@@ -4,7 +4,8 @@ import com.google.gson.JsonObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -27,7 +28,9 @@ class StatisticsTest {
 
     @Test
     void podcastsDownloadsByCityTest() {
-        String expectedString = "Most popular show is: Who Trolled Amber\nNumber of downloads is: 24";
+        String expectedString = """
+                Most popular show is: Who Trolled Amber
+                Number of downloads is: 24""";
 
         JsonObject listOfPodcastsByDownloads = stats.podcastsDownloadsByCity(listOfPodcasts, "San Francisco");
         String mostPopularShow = stats.mostPopularShowByDownloads(listOfPodcastsByDownloads);
@@ -37,9 +40,10 @@ class StatisticsTest {
     }
 
     @Test
-
     void podcastsDownloadsByDeviceTest() {
-        String expectedString = "Most popular device is: mobiles & tablets\nNumber of downloads is: 60";
+        String expectedString = """
+                Most popular device is: mobiles & tablets
+                Number of downloads is: 60""";
         JsonObject downloadsByDevice = stats.podcastsDownloadsByDevice(listOfPodcasts);
         String mostPopularDevice = stats.mostPopularDeviceByDownloads(downloadsByDevice);
         System.out.println(mostPopularDevice);
@@ -51,10 +55,10 @@ class StatisticsTest {
     @Test
     void listTheOpportunitiesToInsertCommercial(){
         String expectedString = """
-                                    Show Id: Stuff You Should Know, Preroll Opportunity Number: 40
-                                    Show Id: Who Trolled Amber, Preroll Opportunity Number: 40
-                                    Show Id: Crime Junkie, Preroll Opportunity Number: 30
-                                    Show Id: The Joe Rogan Experience, Preroll Opportunity Number: 10
+                                Show Id: Stuff You Should Know, Preroll Opportunity Number: 40
+                                Show Id: Who Trolled Amber, Preroll Opportunity Number: 40
+                                Show Id: Crime Junkie, Preroll Opportunity Number: 30
+                                Show Id: The Joe Rogan Experience, Preroll Opportunity Number: 10
                                 """;
 
         JsonObject opportunitiesToInsertCommercial = stats.opportunitiesToInsertCommercial(listOfPodcasts);
@@ -66,6 +70,22 @@ class StatisticsTest {
 
     @Test
     void returnWeeklyShowsDayAndHour(){
-        stats.getWeeklyShowsDetails(listOfPodcasts);
+        String expectedString = """
+                Weekly shows are:
+                
+                Crime Junkie - Wed 22:00
+                Who Trolled Amber - Mon 20:00
+                """;
+
+        Set showsList = stats.getWeeklyShowsDetails(listOfPodcasts);
+
+        ArrayList<String> showsSordedList = new ArrayList<>(showsList);
+        Collections.sort(showsSordedList);
+
+        StringBuilder formatedOutput = new StringBuilder("Weekly shows are:\n\n");
+        for(int i = 0; i < showsSordedList.size(); i++){
+            formatedOutput.append(showsSordedList.get(i)).append("\n");
+        }
+        assertEquals(expectedString, formatedOutput.toString());
     }
 }
